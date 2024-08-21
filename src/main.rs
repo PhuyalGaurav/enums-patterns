35ba35e6678e3dc46f3ad9b078e622317cafbac1
gauravphuyal <phuyalgaurav90@gmail.com>
2024-08-22 00:12:12 +0545

    Add Coin Enum

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,28 @@
-enum IpAddrKind {
-    V4(u8,u8,u8,u8),
-    V6(String),
+fn main(){
+    let manish_money: Coin = Coin::Dime;
+    let himal_money: Coin = Coin::Penny;
 
-}
-
-enum  Message {
-    Quit,
-    Move {x: i32, y:i32},
-    Write(String),
-    ColorRGB(u8, u8, u8)
-}
-
-impl Message {
-    fn some_function(&self) {
-        println!("Ok Blud.");
+    if(value_in_cents(manish_money) > value_in_cents(himal_money)){
+        println!("Manish is Richer than himal");
+    }
+    else {
+        println!("Himal is richer than Manish");
     }
 }
 
-fn main() {
-    let localhost: IpAddrKind = IpAddrKind::V4(127, 0 ,0, 1);
-    let six: IpAddrKind = IpAddrKind::V6(String::from("121.12132.sd23e3r.3r32r"));
-    let user_move: Message = Message::Move { x: 2, y: 4 };
-    user_move.some_function();
+enum Coin {
+    Penny,
+    Nickel, 
+    Dime, 
+    Quarter,
+}
 
-    let some_number: Option<i32> = Some(5);
-    let some_char:Option<char> = Some('e');
-
-    let absent_number: Option<i32>= None;
-
-    let sum_numbers: i32 = some_number.unwrap_or(0) + absent_number.unwrap_or(0);
-
-    println!("The Sum is {}", sum_numbers)
-
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
 }
 
